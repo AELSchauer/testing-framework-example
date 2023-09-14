62f8bb820a7fe5c5714cd109421ef969dda3ec33
Ashley Schauer <ashley.schauer@givecampus.com>
2023-09-14 15:18:15 -0600

    x

--- app/models/user_behavior_tracking.rb
+++ app/models/user_behavior_tracking.rb
@@ -1,4 +1,6 @@
 class UserBehaviorTracking < ApplicationRecord
   belongs_to :user_session
   belongs_to :trackable, polymorphic: true, optional: true
+
+  EVENT_NAMES = %i[create_donation visit_donation_page visit_signup_page].freeze
 end
