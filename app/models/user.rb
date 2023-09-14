d7f9439c549c73b4414e6c6b3fd51f28666d4fe0
Ashley Schauer <ashley.schauer@givecampus.com>
2023-09-14 15:39:28 -0600

    add signup functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_many :user_sessions
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  before_save { self.email = email.downcase }
 end
